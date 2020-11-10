deaf426f57457099f2704591255388c7fc9d3c87
Christian Mena <christian.91.gm@hotmail.com>
2020-11-10 12:36:41 -0500

    CirlcleAvatar Component

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -7,7 +7,36 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
-        backgroundColor: Color(colorApplication)
+        backgroundColor: Color(colorApplication),
+        actions: <Widget>[
+
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg'),
+              radius: 20.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.black,
+              foregroundColor: Colors.white,
+            ),
+          )
+        ],
+        ),
+        body: Center(
+          child: Card(
+            clipBehavior: Clip.antiAlias,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+            child: FadeInImage(
+              image: NetworkImage('https://maguared.gov.co/wp-content/uploads/2015/08/totoro.jpg'),
+              placeholder: AssetImage('assets/load-spinner.gif'),
+              fadeInDuration: Duration(milliseconds: 200),
+            ),
+          ),
         ),
     );
   }
