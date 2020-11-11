4cc5fc13276534e93bd4667e01b0a24f2dd386ea
Christian Mena <christian.91.gm@hotmail.com>
2020-11-11 14:17:46 -0500

    Checkbox and Switch Component

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -7,12 +7,11 @@ class SliderPage extends StatefulWidget {
 }
 
 class _SliderPageState extends State<SliderPage> {
-
   double _valueSlider = 150.0;
+  bool _blockCheck = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         title: Text('Slider'),
         backgroundColor: Color(colorApplication),
@@ -22,37 +21,52 @@ class _SliderPageState extends State<SliderPage> {
         child: Column(
           children: [
             _createSlider(),
-            Expanded(
-              child: _createImage()
-            )
+            _createCheckBox(),
+            _createSwitch(),
+            Expanded(child: _createImage())
           ],
         ),
       ),
-      
     );
   }
 
   Widget _createSlider() {
     return Slider(
-      activeColor: Colors.indigoAccent,
-      label: 'Image Size',
-      value: _valueSlider,
-      min: 10.0,
-      max: 400.0, 
-      onChanged: (value)=>
-        setState(() {
-          _valueSlider=value;
-        })
-    );
-     
+        activeColor: Colors.indigoAccent,
+        label: 'Image Size',
+        value: _valueSlider,
+        min: 10.0,
+        max: 400.0,
+        onChanged: (_blockCheck)? null: (value) => setState(() {
+              _valueSlider = value;
+            }));
   }
 
   Widget _createImage() {
     return FadeInImage(
       placeholder: AssetImage('assets/load-spinner.gif'),
-      image: NetworkImage('https://c0.klipartz.com/pngpicture/81/5/gratis-png-totoro-art-catbus-t-shirt-studio-ghibli-anime-pintura-totoro.png'),
+      image: NetworkImage(
+          'https://c0.klipartz.com/pngpicture/81/5/gratis-png-totoro-art-catbus-t-shirt-studio-ghibli-anime-pintura-totoro.png'),
       width: _valueSlider,
       fit: BoxFit.contain,
     );
+  }
+
+  Widget _createCheckBox() {
+    return CheckboxListTile(
+      title: Text('Block Slider'),
+      value: _blockCheck,
+      onChanged:(value) => setState(() {
+              _blockCheck = value;
+    }));
+  }
+
+  Widget _createSwitch() {
+    return SwitchListTile(
+      title: Text('Switch Slider'),
+      value: _blockCheck,
+      onChanged:(value) => setState(() {
+              _blockCheck = value;
+    }));
   }
 }
